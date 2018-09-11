e7d5d08e120f255699ac75dc3eb84339f4b652df
Alexandre <alexandrebouttier@gmail.com>
2018-09-11 16:58:59 +0200

    create table bet_simple

---
+++ database/migrations/2018_09_11_144711_create_bet_simple_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBetSimpleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bet_simple', function (Blueprint $table) {
+            $table->increments('id');
+            $table->foreign('id')
+				  ->references('id')
+				  ->on('user');
+            $table->string('event');
+            $table->string('sport');
+            $table->string('competition');
+            $table->date('date_event');
+            $table->time('hour_event');
+            $table->string('type');
+            $table->string('stake');
+            $table->string('cost');
+            $table->string('prognosis');
+            $table->string('result');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bet_simple');
+       
+    }
+}
