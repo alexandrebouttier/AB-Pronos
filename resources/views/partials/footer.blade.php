6acaf2be24fce4aea85af8fb36ab1bb07d82981e
Alexandre <alexandrebouttier@gmail.com>
2018-09-21 23:30:57 +0200

    indent

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,8 +1,8 @@
 <footer>
     <div class="container">
-    <p class="text-center" style="margin-top:2em; margin-bottom:1em;"> ©ABPRONOSTICS, 2018 - Tous droits réservés. Sité réalisé
-     par <a href="https://www.alexandrebouttier.fr">Alexandre Bouttier</a></p>
+        <p class="text-center" style="margin-top:2em; margin-bottom:1em;"> ©ABPRONOSTICS, 2018 - Tous droits réservés.
+            Sité réalisé
+            par <a href="https://www.alexandrebouttier.fr">Alexandre Bouttier</a></p>
     </div>
-    
-    </footer>
-    
+
+</footer>
