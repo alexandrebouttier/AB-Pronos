0a07f461331c48cfca83f9db928fcf036244a9eb
Alexandre <alexandrebouttier@gmail.com>
2018-09-21 23:09:58 +0200

    fix route login

--- routes/web.php
+++ routes/web.php
@@ -9,17 +9,14 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', 'HomeController@home');
 
 Auth::routes();
-
+Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout');
 Route::get('/pronostics', 'BetsController@index')->name('bets');
 
 Route::get('/contact', function () {
     return view('pages/contact');
 });
-
-
-
