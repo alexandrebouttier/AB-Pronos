dfd6fd12cf0610074d74cb030db555aa8f0c83f0
Alexandre <alexandrebouttier@gmail.com>
2018-09-21 23:26:56 +0200

    change copyright

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,6 +1,6 @@
 <footer>
     <div class="container">
-    <p class="text-center" style="margin-top:2em; margin-bottom:1em;"> ©TEAM-BET, 2018 - Tous droits réservés. Sité réalisé
+    <p class="text-center" style="margin-top:2em; margin-bottom:1em;"> ©ABPRONOSTICS, 2018 - Tous droits réservés. Sité réalisé
      par <a href="https://www.alexandrebouttier.fr">Alexandre Bouttier</a></p>
     </div>
     
