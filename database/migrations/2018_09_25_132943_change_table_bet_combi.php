35e6380fa7c287d99df9ca4b4387f00331d144ae
Alexandre Bouttier <alexandrebouttier@gmail.com>
2018-09-25 15:30:24 +0200

    edit table bet_combi

--- database/migrations/2018_09_25_132943_change_table_bet_combi.php
+++ database/migrations/2018_09_25_132943_change_table_bet_combi.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTableBetCombi extends Migration
+class ChangeTableBetCombi extends Migration
 {
     /**
      * Run the migrations.
