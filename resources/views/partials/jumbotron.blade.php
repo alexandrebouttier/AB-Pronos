27235442bf4e4254bb39e0e75070fc4fe56ee31e
Alexandre <alexandrebouttier@gmail.com>
2018-10-02 17:10:18 +0200

    add img banner jumbotron

--- resources/views/partials/jumbotron.blade.php
+++ resources/views/partials/jumbotron.blade.php
@@ -4,9 +4,9 @@
 
     <div class="container">
 
-        <h1>ABPronos</h1>
-        <p class="lead">Je partage & analyse pour vous ,des dizaines de matchs pour dénicher les matchs à gros potentiel. Je mets ainsi à votre disposition mes pronostics de paris sportifs , en vous proposant mes sélections football qui vous feront enfin gagner.
-            <br>Un seul objectif : VOUS FAIRE GAGNER ! .
+        <h1><mark>ABPronos</mark> </h1>
+        <p style="margin-top:1em;"class="lead"><mark>Je partage & analyse pour vous ,des dizaines de matchs pour dénicher les matchs à gros potentiel. Je mets ainsi à votre disposition mes pronostics de paris sportifs , en vous proposant mes sélections football qui vous feront enfin gagner.
+            <br>Un seul objectif : VOUS FAIRE GAGNER ! .</mark>
         </p>
     </div>
 
