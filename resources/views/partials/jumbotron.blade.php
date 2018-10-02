2e8abb40e24f87b839bcaf79a68ed4b345e8fffe
Alexandre <alexandrebouttier@gmail.com>
2018-10-02 18:05:47 +0200

    update text

--- resources/views/partials/jumbotron.blade.php
+++ resources/views/partials/jumbotron.blade.php
@@ -5,7 +5,7 @@
     <div class="container">
 
         <h1><mark>ABPronos</mark> </h1>
-        <p style="margin-top:1em;"class="lead"><mark>Je partage & analyse pour vous ,des dizaines de matchs pour dénicher les matchs à gros potentiel. Je mets ainsi à votre disposition mes pronostics de paris sportifs , en vous proposant mes sélections football qui vous feront enfin gagner.
+        <p style="margin-top:1em;"class="lead"><mark>Je partage & analyse pour vous , des dizaines de matchs pour dénicher les matchs à gros potentiel.<br> Je mets ainsi à votre disposition mes pronostics de paris sportifs , en vous proposant mes sélections sur le football.
             <br>Un seul objectif : VOUS FAIRE GAGNER ! .</mark>
         </p>
     </div>
