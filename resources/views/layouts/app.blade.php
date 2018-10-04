dd4cad1a27eb39269d2bacaf331d332cd4fbbac7
Alexandre Bouttier <alexandrebouttier@gmail.com>
2018-10-04 11:41:54 +0200

    add zopim tchat

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,15 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+    <!--Start of Zendesk Chat Script-->
+<script type="text/javascript">
+    window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
+    d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+    _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute("charset","utf-8");
+    $.src="https://v2.zopim.com/?1W6cc8maSCCal1ekkGLp9JS1ndCm5jHp";z.t=+new Date;$.
+    type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");
+    </script>
+    <!--End of Zendesk Chat Script-->
     <!-- Hotjar Tracking Code for https://www.teambet.fr -->
 <script>
         (function(h,o,t,j,a,r){
@@ -22,7 +31,7 @@
     <title>ABPronos - Pronostiqueur en paris sportifS</title>
 
     <!-- Scripts -->
-  
+
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
